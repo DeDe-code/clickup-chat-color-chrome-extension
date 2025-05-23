685d7b9658bb6e8a3ad894c659e4b26f567fb6b2
Derzsi Dezső <derzsidezso@gmail.com>
2025-05-23 12:05:20 +0300

    basic temporary design added

--- src/components/ColorManager.vue
+++ src/components/ColorManager.vue
@@ -9,10 +9,31 @@ const { backgroundColor, textColor } = useColorPicker('#ffff', '#000', (colors) 
 })
 </script>
 <template>
-  <div class="background-picker-wrapper">
-    <color-picker class="test" v-model:pureColor="backgroundColor" />
-  </div>
-  <div class="background-picker-wrapper">
-    <color-picker class="test" v-model:pureColor="textColor" />
+  <div class="color-picker-wrapper">
+    <div class="background-picker-wrapper">
+      <label for="background-picker">Background</label>
+      <color-picker id="background-picker" v-model:pureColor="backgroundColor" />
+    </div>
+    <div class="text-picker-wrapper">
+      <label for="text-picker">Text</label>
+      <color-picker id="text-picker" v-model:pureColor="textColor" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.color-picker-wrapper {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+}
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+::v-deep(.vc-color-wrap) {
+  width: 100%;
+  height: 2rem;
+  border: 1px solid red;
+}
+</style>
--- src/views/UmcExtentionView.vue
+++ src/views/UmcExtentionView.vue
@@ -1,6 +1,6 @@
 <script setup>
 /* global chrome */
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useColorStore } from '../stores/ColorStore.js'
 import ColorManager from '../components/ColorManager.vue'
@@ -8,25 +8,52 @@ import ColorPreview from '../components/ColorPreview.vue'
 
 const colorStore = useColorStore()
 const { backgroundColor, textColor } = storeToRefs(colorStore)
+const isReady = ref('false')
 
 onMounted(() => {
-  chrome.storage.local.get(['backgroundColor', 'textColor'], ({ backgroundColor, textColor }) => {
-    if (backgroundColor && textColor) {
-      colorStore.setColors(backgroundColor, textColor)
-    }
-  })
+  // chrome.storage.local.get(['backgroundColor', 'textColor'], ({ backgroundColor, textColor }) => {
+  //   if (backgroundColor && textColor) {
+  //     colorStore.setColors(backgroundColor, textColor)
+  //   }
+  // isReady.value = true
+  // })
+  isReady.value = true
 })
 
 const handleColorsChanged = ({ backgroundColor: bg, textColor: txt }) => {
   colorStore.setColors(bg, txt)
-  chrome.storage.local.set({
-    backgroundColor: bg,
-    textColor: txt,
-  })
+  // chrome.storage.local.set({
+  //   backgroundColor: bg,
+  //   textColor: txt,
+  // })
 }
 </script>
 <template>
-  <div>welcome from extention</div>
-  <ColorManager @colorsChanged="handleColorsChanged" />
-  <ColorPreview :backgroundColor="backgroundColor" :textColor="textColor" />
+  <main class="extension-wrapper">
+    <div v-if="isReady" class="extension-tools-wrapper">
+      <h1>Chose your colors</h1>
+      <ColorManager @colorsChanged="handleColorsChanged" />
+      <ColorPreview :backgroundColor="backgroundColor" :textColor="textColor" />
+    </div>
+    <div v-else class="loading-wrapper">loading...</div>
+  </main>
 </template>
+
+<style scoped>
+/* .extension-wrapper {
+  max-width: 40%;
+  margin: 0 auto;
+  padding: 1rem;
+  color: #000;
+  background-color: #fff;
+  border: 1px solid red;
+} */
+.extension-tools-wrapper {
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+}
+.extension-wrapper h1 {
+  text-transform: capitalize;
+}
+</style>
